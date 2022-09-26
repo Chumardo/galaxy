1905115f8e7dd7a64476001efb54bb2c4aa629b3
chumardo <dachigiorgadze98@gmail.com>
2022-09-26 18:28:56 +0400

    display a tile

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, Clock
 from kivy.graphics.context_instructions import Color
-from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.vertex_instructions import Line, Quad
 from kivy.core.window import Window
 from kivy import platform
 
@@ -32,11 +32,16 @@ class MainWidget(Widget):
     current_speed_x = 0
     current_offset_x = 0
 
+    tile = None
+    ti_x = 0
+    ti_y = 0
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print("INIT W: " +str(self.width) + "H: " + str(self.height))
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        self.init_tiles()
 
         if self.is_desktop():
             self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
@@ -50,6 +55,12 @@ class MainWidget(Widget):
         if platform in ('linux', 'win', 'macosx'):
             return True
         return False
+
+    def init_tiles(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            self.tile = Quad()
+
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -69,6 +80,25 @@ class MainWidget(Widget):
         spacing_y = self.H_LINES_SPACING * self.height
         line_y = index * spacing_y - self.current_offset_y
         return line_y
+
+
+    def get_tile_coordinates(self, ti_x, ti_y):
+        x = self.get_line_x_from_index(ti_x)
+        y = self.get_line_y_from_index(ti_y)
+        return x, y
+    
+    def update_tiles(self):
+        xmin, ymin = self.get_tile_coordinates(self.ti_x, self.ti_y)
+        xmax, ymax = self.get_tile_coordinates(self.ti_x + 1, self.ti_y + 1)
+
+        x1, y1 = self.transform(xmin, ymin)
+        x2, y2 = self.transform(xmin, ymax)
+        x3, y3 = self.transform(xmax, ymax)
+        x4, y4 = self.transform(xmax, ymin)
+
+
+        self.tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
+
 
 
     def update_vertical_lines(self):
@@ -91,7 +121,7 @@ class MainWidget(Widget):
         end_index = start_index + self.V_NB_LINES - 1
         xmin = self.get_line_x_from_index(start_index)
         xmax = self.get_line_x_from_index(end_index)
-        
+
         for i in range(0, self.H_NB_LINES):
             line_y = self.get_line_y_from_index(i)
             x1, y1 = self.transform(xmin, line_y)
@@ -103,6 +133,7 @@ class MainWidget(Widget):
         time_factor = dt*60
         self.update_vertical_lines()
         self.update_horizontal_lines()
+        self.update_tiles()
         self.current_offset_y += self.SPEED * time_factor
 
 
