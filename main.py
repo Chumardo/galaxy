8487c900109955869e173ffff9c6663ba92d6207
chumardo <dachigiorgadze98@gmail.com>
2022-09-27 17:57:24 +0400

    restart the game

--- main.py
+++ main.py
@@ -62,8 +62,7 @@ class MainWidget(RelativeLayout):
         self.init_horizontal_lines()
         self.init_tiles()
         self.init_ship()
-        self.pre_fill_tiles_coordinates()
-        self.generate_tiles_coordinates()
+        self.reset_game()
 
         if self.is_desktop():
             self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
@@ -72,6 +71,22 @@ class MainWidget(RelativeLayout):
 
 
         Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def reset_game(self):
+        self.current_offset_y = 0
+        self.current_y_loop = 0
+        self.current_speed_x = 0
+        self.current_offset_x = 0
+        
+
+        self.tiles_coordinates = []
+        self.pre_fill_tiles_coordinates()
+        self.generate_tiles_coordinates()
+
+
+        self.state_game_over = False
+
+
 
     def is_desktop(self):
         if platform in ('linux', 'win', 'macosx'):
@@ -264,6 +279,7 @@ class MainWidget(RelativeLayout):
 
     def on_menu_button_pressed(self):
         print("BUTTON")
+        self.reset_game()
         self.state_game_has_started = True
         self.menu_widget.opacity = 0
 
