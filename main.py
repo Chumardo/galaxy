dbcc827aa05864e180e86ded9dc97864c2bde4a6
chumardo <dachigiorgadze98@gmail.com>
2022-09-27 17:29:26 +0400

    game over state

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class MainWidget(Widget):
     H_LINES_SPACING = .1
     horizontal_lines = []
 
-    SPEED = .1
+    SPEED = .4
     current_offset_y = 0
     current_y_loop = 0
 
@@ -45,6 +45,8 @@ class MainWidget(Widget):
     SHIP_BASE_Y = 0.04
     ship = None
     ship_coordinates = [(0, 0), (0, 0), (0, 0)]
+
+    state_game_over = False
 
 
 
@@ -115,7 +117,6 @@ class MainWidget(Widget):
         with self.canvas:
             Color(1, 1, 1)
             for i in range(0, self.NB_TILES):
-
                 self.tiles.append(Quad())
 
     def pre_fill_tiles_coordinates(self):
@@ -238,20 +239,21 @@ class MainWidget(Widget):
         self.update_tiles()
         self.update_ship()
 
-        speed_y = self.SPEED * self.height / 100
-        self.current_offset_y += speed_y * time_factor
-        self.generate_tiles_coordinates()
+        if not self.state_game_over:
+            speed_y = self.SPEED * self.height / 100
+            self.current_offset_y += speed_y * time_factor
 
-        spacing_y = self.H_LINES_SPACING * self.height
-        if self.current_offset_y >= spacing_y:
-            self.current_offset_y -= spacing_y
-            self.current_y_loop += 1
+            spacing_y = self.H_LINES_SPACING * self.height
+            while self.current_offset_y >= spacing_y:
+                self.current_offset_y -= spacing_y
+                self.current_y_loop += 1
         
 
-        speed_x = self.current_speed_x * self.width / 100
-        self.current_offset_x += self.current_speed_x * time_factor
+            speed_x = self.current_speed_x * self.width / 100
+            self.current_offset_x += self.current_speed_x * time_factor
 
-        if not self.check_ship_collision():
+        if not self.check_ship_collision() and not self.state_game_over:
+            self.state_game_over = True
             print("GAME OVER")
 
 
