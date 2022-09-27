ffb4828315de436aa9120fba686ab0af5834ed77
chumardo <dachigiorgadze98@gmail.com>
2022-09-27 12:44:50 +0400

    display several tiles

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ class MainWidget(Widget):
     current_speed_x = 0
     current_offset_x = 0
 
-    tile = None
-    ti_x = 0
-    ti_y = 0
+    NB_TILES = 4
+    tiles = []
+    tiles_coordinates = []
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -43,6 +43,7 @@ class MainWidget(Widget):
         self.init_vertical_lines()
         self.init_horizontal_lines()
         self.init_tiles()
+        self.generate_tiles_coordinates()
 
         if self.is_desktop():
             self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
@@ -60,7 +61,14 @@ class MainWidget(Widget):
     def init_tiles(self):
         with self.canvas:
             Color(1, 1, 1)
-            self.tile = Quad()
+            for i in range(0, self.NB_TILES):
+
+                self.tiles.append(Quad())
+
+    def generate_tiles_coordinates(self):
+        for i in range(0, self.NB_TILES):
+            self.tiles_coordinates.append((0, i))
+
 
 
     def init_vertical_lines(self):
@@ -90,16 +98,19 @@ class MainWidget(Widget):
         return x, y
     
     def update_tiles(self):
-        xmin, ymin = self.get_tile_coordinates(self.ti_x, self.ti_y)
-        xmax, ymax = self.get_tile_coordinates(self.ti_x + 1, self.ti_y + 1)
+        for i in range(0, self.NB_TILES):
+            tile = self.tiles[i]
+            tile_coordinate = self.tiles_coordinates[i]
+            xmin, ymin = self.get_tile_coordinates(tile_coordinate[0], tile_coordinate[1])
+            xmax, ymax = self.get_tile_coordinates(tile_coordinate[0] + 1, tile_coordinate[1] + 1)
 
-        x1, y1 = self.transform(xmin, ymin)
-        x2, y2 = self.transform(xmin, ymax)
-        x3, y3 = self.transform(xmax, ymax)
-        x4, y4 = self.transform(xmax, ymin)
+            x1, y1 = self.transform(xmin, ymin)
+            x2, y2 = self.transform(xmin, ymax)
+            x3, y3 = self.transform(xmax, ymax)
+            x4, y4 = self.transform(xmax, ymin)
 
 
-        self.tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
+            tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
 
 
 
